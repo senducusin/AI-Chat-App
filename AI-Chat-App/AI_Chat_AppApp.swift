2c60579730b277dd81b8d802955176a3f0dbdd49
Jansen Ducusin <senducusin@gmail.com>
2025-07-21 00:57:28 +0800

    Initial Commit

---
+++ AI-Chat-App/AI_Chat_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Chat_AppApp.swift
+//  AI-Chat-App
+//
+//  Created by Jansen Ducusin on 7/21/25.
+//
+
+import SwiftUI
+
+@main
+struct AI_Chat_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI-Chat-AppTests/AI_Chat_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Chat_AppTests.swift
+//  AI-Chat-AppTests
+//
+//  Created by Jansen Ducusin on 7/21/25.
+//
+
+import Testing
+@testable import AI_Chat_App
+
+struct AI_Chat_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
