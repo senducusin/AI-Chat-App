c8448880163ea4bb29cf58dcfdfe1b7f884f3857
Jansen Ducusin <senducusin@gmail.com>
2025-07-21 22:39:57 +0800

    [Feat] Fix logout animation

--- AI-Chat-App/Core/Settings/SettingsView.swift
+++ AI-Chat-App/Core/Settings/SettingsView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @Environment(\.dismiss) private var dismiss
     @Environment(AppState.self) private var appState
 
     var body: some View {
@@ -24,7 +25,12 @@ struct SettingsView: View {
     }
 
     func onSignOutPressed() {
-        appState.updateViewState(showTabBarView: false)
+        dismiss()
+
+        Task {
+            try? await Task.sleep(for: .seconds(1))
+            appState.updateViewState(showTabBarView: false)
+        }
     }
 }
 
